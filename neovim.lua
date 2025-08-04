fcb2342424a932cf21aaef6778d50de783f6863a
Justin Lowry <j.lowry@hey.com>
2025-08-04 15:08:05 -0600

    update cursor text for legibility

--- neovim.lua
+++ neovim.lua
@@ -1,19 +1,9 @@
 return {
-	{
-		"ribru17/bamboo.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("bamboo").setup({
-				style = "multiplex",
-			})
-			require("bamboo").load()
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "bamboo",
-		},
-	},
+	"ribru17/bamboo.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("bamboo").setup({})
+		require("bamboo").load()
+	end,
 }
