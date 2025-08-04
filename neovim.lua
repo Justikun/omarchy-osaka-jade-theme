5ebac44de5713ccdc5d60512b3c64b2d7dae822b
Justin Lowry <j.lowry@hey.com>
2025-08-04 01:33:12 -0600

    update theme to ribru17/bamboo

--- neovim.lua
+++ neovim.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"Justikun/osaka-jade",
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "osaka-jade",
-		},
-	},
+	"ribru17/bamboo.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("bamboo").setup({
+			style = "multiplex",
+		})
+		require("bamboo").load()
+	end,
 }
